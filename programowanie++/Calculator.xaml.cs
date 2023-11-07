52736c490e9106b21cc176af292884d070c98fd5
Bohdan Velikdus <140083@student.uni.opole.pl>
2023-11-07 11:16:07 -0800

    Lista 2/ XO, statki

---
+++ programowanie++/Calculator.xaml.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+
+namespace programowanie__
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Window
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private List<Button> lastClickedButtons = new List<Button>();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                string buttonText = button.Content.ToString();
+                Output.Text += $"{buttonText}";
+                lastClickedButtons.Add(button);
+            }
+
+        }
+
+        private void ButtonZnak_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                string buttonText = button.Content.ToString();
+                if (lastClickedButtons.Count == 0)
+                {
+                    switch (buttonText)
+                    {
+                        case "*":
+                        case "/":
+                        case "+":
+                            return;
+                        case "-":
+                            Output.Text += $"{buttonText}";
+                            break;
+                    }
+                }
+                //refactoring
+                else
+                {
+                    if (!(/*lastClickedButtons.Count > 0 &&*/ lastClickedButtons[lastClickedButtons.Count - 1].Tag.ToString() == "Znak"))
+                    {
+                        Output.Text += $"{buttonText}";
+                        lastClickedButtons.Add(button);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+
+
+
+            }
+        }
+
+        public static string BalanceBrackets(string input)
+        {
+            int bracketCount = 0;
+            StringBuilder balancedStringBuilder = new StringBuilder(input);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    bracketCount++;
+                }
+                else if (input[i] == ')')
+                {
+                    bracketCount--;
+                    if (bracketCount < 0)
+                    {
+                        // Insert a closing bracket at the appropriate position
+                        balancedStringBuilder.Insert(i, ")");
+                        bracketCount = 0; // Reset bracket count
+                    }
+                }
+            }
+
+            // Add any missing closing brackets at the end
+            for (int i = 0; i < bracketCount; i++)
+            {
+                balancedStringBuilder.Append(")");
+            }
+
+            return balancedStringBuilder.ToString();
+        }
+
+        private void Button_Calc(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string expression = Output.Text;
+                expression = BalanceBrackets(expression);
+                DataTable table = new DataTable();
+                table.Columns.Add("expression", typeof(string), expression);
+                DataRow row = table.NewRow();
+                table.Rows.Add(row);
+                double result = double.Parse((string)row["expression"]);
+                Output.Text = result.ToString();
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Division by zero!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected exception");
+            }
+
+
+        }
+
+        private void Button_Clear(object sender, RoutedEventArgs e)
+        {
+            string toEr1char = Output.Text;
+            if (toEr1char.Length == 0)
+            {
+                return;
+            }
+            else
+            {
+                Output.Text = toEr1char.Remove(toEr1char.Length - 1);
+                lastClickedButtons.RemoveAt(lastClickedButtons.Count - 1);
+
+            }
+        }
+
+        private void Button_floatPoint(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (lastClickedButtons.Count == 0 || (lastClickedButtons[lastClickedButtons.Count - 1].Tag.ToString() != "Numer" && lastClickedButtons[lastClickedButtons.Count - 1].Tag.ToString() != "floatPoint"))
+                {
+                    Output.Text += "0.";
+                    lastClickedButtons.Add(num);
+                    lastClickedButtons.Add(button);
+                }
+                //refactoring too
+                else
+                {
+                    bool metFloatPoint = false;
+                    for (int i = lastClickedButtons.Count - 1; i >= 0; i--)
+                    {
+                        if (lastClickedButtons[i].Tag.ToString() == "Znak")
+                        {
+                            break;
+                        }
+                        else
+                        if (lastClickedButtons[i].Tag.ToString() == "floatPoint")
+                        {
+                            metFloatPoint = !metFloatPoint;
+                            break;
+                        }
+                    }
+
+                    if (metFloatPoint)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        lastClickedButtons.Add(button);
+                        Output.Text += ".";
+                    }
+
+
+                }
+
+
+
+            }
+        }
+
+        private void braket_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                Output.Text += button.Content.ToString();
+            }
+
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            Output.Text = "";
+            lastClickedButtons.Clear();
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.D0:
+                case Key.D1:
+                case Key.D2:
+                case Key.D3:
+                case Key.D4:
+                case Key.D5:
+                case Key.D6:
+                case Key.D7:
+                case Key.D8:
+                case Key.D9:
+                    Output.Text += e.Key.ToString()[1];
+                    lastClickedButtons.Add(num);
+                    break;
+            }
+        }
+    }
+}
---
+++ programowanie++/TICTACTOE.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace programowanie__
+{
+    /// <summary>
+    /// Interaction logic for TICTACTOE.xaml
+    /// </summary>
+    public partial class TICTACTOE : Window
+    {
+
+        public class GameLogic { 
+            
+            public string[,] gameField = new string[3,3];
+            public bool turnX;
+                
+            public GameLogic() {
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        gameField[row, col] = "";
+                    }
+                }
+                turnX = true;
+            }
+
+            public void turnDone() {
+                turnX = !turnX;
+            }
+
+            public bool checkWin() {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (gameField[i, 0] != "" && gameField[i, 0] == gameField[i, 1] && gameField[i, 1] == gameField[i, 2])
+                        return true; // Winning row
+
+                    if (gameField[0, i] != "" && gameField[0, i] == gameField[1, i] && gameField[1, i] == gameField[2, i])
+                        return true; // Winning column
+                }
+
+                // Check diagonals for a win
+                if (gameField[0, 0] != "" && gameField[0, 0] == gameField[1, 1] && gameField[1, 1] == gameField[2, 2])
+                    return true; // Winning diagonal
+
+                if (gameField[0, 2] != "" && gameField[0, 2] == gameField[1, 1] && gameField[1, 1] == gameField[2, 0])
+                    return true; // Winning diagonal
+
+                return false; // No winner yet
+            }
+
+        }
+
+             
+        public GameLogic gameLogic;
+        
+        private void CreateGrid()
+        {
+            gameLogic = new GameLogic();
+            // Создаем новый Grid
+            Grid grid = new Grid();
+
+            // Добавляем строки и столбцы в сетку (3x3)
+            for (int i = 0; i < 3; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            // Добавляем кнопки в ячейки Grid
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Button button = new Button
+                    {
+                        Content = $"Button {row}-{col}",
+                        HorizontalAlignment = HorizontalAlignment.Stretch,
+                        VerticalAlignment = VerticalAlignment.Stretch,
+                        IsEnabled = true,
+                            
+                    };
+                    
+                    button.Click += makeTurn;
+                    Binding binding = new Binding(gameLogic.gameField[row, col]);
+
+                    button.SetBinding(Button.ContentProperty, binding);
+                    Grid.SetRow(button, row); // Устанавливаем строку для кнопки
+                    Grid.SetColumn(button, col); // Устанавливаем столбец для кнопки
+                    grid.Children.Add(button); // Добавляем кнопку в Grid
+                }
+            }
+
+            // Добавляем созданный Grid на окно
+            Content = grid;
+
+
+        }
+
+        private void makeTurn(object sender, RoutedEventArgs e)
+        {
+            Button bt = (Button)sender;
+            int col = Grid.GetColumn(bt);
+            int row = Grid.GetRow(bt);
+            MessageBox.Show("col:" + col + "\trow:" + row);
+            gameLogic.gameField[row, col] = (gameLogic.turnX == true) ? "X" : "O";
+            gameLogic.turnDone();
+            bt.IsEnabled = false;
+            if (gameLogic.checkWin()) {
+                MessageBox.Show("win " + ((gameLogic.turnX == true) ? "O":"X"));
+
+            }
+        }
+
+        public TICTACTOE()
+        {
+            
+            InitializeComponent();
+            CreateGrid();
+             
+        }
+    }
+}
