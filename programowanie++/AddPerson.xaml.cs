641ae7b01b8237327ac9af66e9796e8bb668a96b
Bohdan Velikdus <140083@student.uni.opole.pl>
2023-11-28 12:21:51 -0800

    Lista 3

---
+++ programowanie++/AddPerson.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace programowanie__
+{
+    /// <summary>
+    /// Interaction logic for AddToXML.xaml
+    /// </summary>
+    public partial class AddPerson : Window
+    {
+        public bool IsOkPressed { get; set; }
+
+
+        public AddPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IsOkPressed = true;
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            IsOkPressed = false;
+            this.Close();
+        }
+    }
+}
---
+++ programowanie++/MainDser.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace programowanie__
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainDser : Window
+    {
+        List<Person> listOfPersons = new List<Person>();
+        string outFilePath = "output2.xml";
+        public MainDser()
+        {
+            InitializeComponent();
+
+            readFile(outFilePath);
+
+            dgSimple.ItemsSource = listOfPersons;
+        }
+
+        public void writeFile(string xmlFilePath)
+        {
+            Serializer.SerializeToXml<List<Person>>(listOfPersons, xmlFilePath);
+        }
+        public void readFile(string xmlFilePath)
+        {
+            listOfPersons = Serializer.DeserializeToObject<List<Person>>(xmlFilePath);
+        }
+
+        public void Button_AddPerson(object sender, RoutedEventArgs e)
+        {
+            //Window1 okno = new Window1();
+            Person person = new Person();
+            AddPerson addPersonDialog = new AddPerson();
+            addPersonDialog.DataContext = person;
+            addPersonDialog.ShowDialog();
+
+            if (addPersonDialog.IsOkPressed)
+            {
+                listOfPersons.Add(person);
+                dgSimple.Items.Refresh();
+                writeFile(outFilePath);
+            }
+        }
+    }
+
+}
